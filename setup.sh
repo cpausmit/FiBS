53d739f7d90febb6beff1fedd9ff665cbd7db198
Christoph Paus <paus@mit.edu>
2016-04-16 21:33:05 -0400

    Small fixes after last re-arrangment and adding more workers.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ export FIBS_BASE=$HOME/FiBS
 export FIBS_WORK=$HOME/cms/work/fibs
 export FIBS_LOGS=$HOME/cms/logs/fibs
 
-export PATH=$PATH:$FIBS_BASE/bin
-export PYTHONPATH=$PYTHONPATH:$FIBS_BASE/python
+export PATH=${PATH}:${FIBS_BASE}/bin
+export PYTHONPATH=${PYTHONPATH}:${FIBS_BASE}/python
