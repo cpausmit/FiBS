f5b37b95be04a809d6bfb812b4ff810e1c9a782d
Christoph Paus <paus@mit.edu>
2016-06-14 16:03:28 -0400

    Allowing command to be executed inside a lock.

--- bin/fibsLock.py
+++ bin/fibsLock.py
@@ -27,11 +27,13 @@ def establishLock(task,debug):
 #===================================================================================================
 # Define string to explain usage of the script
 usage =  " Usage: fibsLock.py   --configFile=<file with full config>\n"
-usage += "                    [ --debug=0 ]             <-- see various levels of debug output\n"
+usage += "                    [ --cmd='' ]          <-- execute OS command inside lock\n"
+usage += "                    [ --seconds=-1 ]      <-- keep lock for a fixed number of seconds\n"
+usage += "                    [ --debug=0 ]         <-- see various levels of debug output\n"
 usage += "                    [ --help ]\n\n"
 
 # Define the valid options which can be specified and check out the command line
-valid = ['configFile=','debug=','help']
+valid = ['configFile=','cmd=','seconds=','debug=','help']
 try:
     opts, args = getopt.getopt(sys.argv[1:], "", valid)
 except getopt.GetoptError, ex:
@@ -43,6 +45,7 @@ except getopt.GetoptError, ex:
 # Get all parameters for the production
 # --------------------------------------------------------------------------------------------------
 # Set defaults for each command line parameter/option
+seconds = -1
 debug = 0
 configFile = ''
 
@@ -53,6 +56,10 @@ for opt, arg in opts:
         sys.exit(0)
     elif opt == "--configFile":
         configFile = arg
+    elif opt == "--seconds":
+        seconds = int(arg)
+    elif opt == "--cmd":
+        cmd = arg
     elif opt == "--debug":
         debug = int(arg)
 
@@ -60,14 +67,27 @@ for opt, arg in opts:
 #--------------------------------
 config = ConfigParser.RawConfigParser()
 config.read(configFile)
+
 # get our parameters as needed
 task = config.get('general','task')
 
 # keeping track of the hostname
 hostname = socket.gethostname()
 
+# establish lock
 lock = establishLock(task,debug)
-answer = raw_input("Type return when done: ")
+
+# now consider our options
+if   cmd != '':
+    print ' Executing: ' + cmd
+    os.system(cmd)
+elif seconds >= 0:
+    time.sleep(seconds)
+else:
+    answer = raw_input("Type return when done: ")
+
+# release the lock
 lock.release()
+print '\n Exiting.'
         
 sys.exit(0)
