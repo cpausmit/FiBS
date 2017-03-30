7f9bd1db4995360312efd3026841080b52a47373
Christoph Paus <paus@mit.edu>
2017-03-30 13:55:28 -0400

    Make more standalone.

--- config/checkFile.bash
+++ config/checkFile.bash
@@ -1,6 +1,6 @@
 # make sure we have a certificate
 voms-proxy-init --valid 168:00 -voms cms >& /dev/null
-source $HOME/Panda/Kraken/setup.sh
+source /home/cmsprod/Tools/Kraken/setup.sh
 source /home/cmsprod/Tools/Dools/setup.sh
 source /home/cmsprod/Tools/T2Tools/setup.sh
 cd -
